91c1b8c3329d9cb2c2241070479fb3a8ade193fd
badal kainth <badalkainth06@gmail.com>
2023-03-23 16:15:00 +0530

    Miku-AI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
+import image from "./data/images/threeChat.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className={classes.homeSection}>
+        <img src={image} alt="Img" className={classes.chatImg}></img>
+        <div className={classes.information}>
+          <h1>Miku-AI</h1>
+          <h3>
+            Use Chat-GPT on your <span>WhatsApp!</span>{" "}
+          </h3>
+          <p>
+            We’ve created Miku-AI, which uses the latest milestone in OpenAI’s
+            effort in scaling up deep learning(GPT-4) and provide answers on
+            WhatsApp directly. GPT-4 is a large multimodal model (accepting
+            image and text inputs, emitting text outputs) that, while less
+            capable than humans in many real-world scenarios, exhibits
+            human-level performance on various professional and academic
+            benchmarks.
+          </p>
+
+          <button className={classes.chatBtn}>TRY ON YOUR WHATSAPP</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
